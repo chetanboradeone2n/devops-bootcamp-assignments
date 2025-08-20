f98ef073465574a1eb6cbf38e3b7258201ad7f8a
Chetan Borade <chetan.borade@one2n.in>
2025-08-20 11:53:51 +0530

    tests: commented the print statements

--- tests/test_students.py
+++ tests/test_students.py
@@ -4,18 +4,18 @@ import requests
 class TestStudentsAPI(unittest.TestCase):
 
     def test_healthcheck(self):
-        print("Testing the healthcheck endpoint")
+#       print("Testing the healthcheck endpoint")
         response = requests.get('http://localhost:5000/api/v1/healthcheck')
-        print(f"Response: {response.status_code}, {response.text}")
-        print(f" Response JSON: {response.json()}")
+#       print(f"Response: {response.status_code}, {response.text}")
+#       print(f" Response JSON: {response.json()}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['status'],'ok')
     
     def test_get_all_students(self):
-        print("testing the get all students endpoint")
+#       print("testing the get all students endpoint")
         response = requests.get('http://localhost:5000/api/v1/students')
-        print(f"Response: {response.status_code}, {response.text}")
-        print(f" Response JSON: {response.json()}")
+#       print(f"Response: {response.status_code}, {response.text}")
+#       print(f" Response JSON: {response.json()}")
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
 
