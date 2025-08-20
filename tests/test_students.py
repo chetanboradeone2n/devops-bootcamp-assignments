63c0d748672c94c332bdf2c70734e9f94ec0f019
Chetan Borade <chetan.borade@one2n.in>
2025-08-20 11:31:01 +0530

    tests: Added the print statement to check if tests gives the 200 output for the healthcheck

--- tests/test_students.py
+++ tests/test_students.py
@@ -4,11 +4,13 @@ import requests
 class TestStudentsAPI(unittest.TestCase):
 
     def test_healthcheck(self):
+        print("Testing the healthcheck endpoint")
         response = requests.get('http://localhost:5000/api/v1/healthcheck')
+        print(f"Response: {response.status_code}, {response.text}")
+        print(f" Response JSON: {response.json()}")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {status: 'ok'})
+        self.assertEqual(response.json()['status'],'ok'})
 
 if __name__ == '__main__':
     unittest.main()
 
-    
