2947105ad0677da906352c9f29d10b907e63b6bf
Chetan Borade <chetan.borade@one2n.in>
2025-08-20 11:47:26 +0530

    tests: added the tests for the get all students endpoint

--- tests/test_students.py
+++ tests/test_students.py
@@ -10,6 +10,14 @@ class TestStudentsAPI(unittest.TestCase):
         print(f" Response JSON: {response.json()}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['status'],'ok')
+    
+    def test_get_all_students(self):
+        print("testing the get all students endpoint")
+        response = requests.get('http://localhost:5000/api/v1/students')
+        print(f"Response: {response.status_code}, {response.text}")
+        print(f" Response JSON: {response.json()}")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
 
 if __name__ == '__main__':
     unittest.main()
