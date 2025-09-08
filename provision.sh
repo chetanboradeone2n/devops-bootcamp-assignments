e5d0857d77b21f42a601f548615d3e07d54a67ff
Chetan Borade <chetan.borade@one2n.in>
2025-09-08 12:38:44 +0530

    chore: added container configuration for database, 2 apis, and nginx

--- provision.sh
+++ provision.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "Provisioning the system..."
+sudo apt-get update -y
 sudo apt-get install docker.io -y
 sudo usermod -aG docker vagrant
 sudo docker --version
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo systemctl status docker
 sudo apt-get install docker-compose -y
 sudo docker-compose --version
+sudo apt-get install curl -y
+echo "success - Provisioning completed"
